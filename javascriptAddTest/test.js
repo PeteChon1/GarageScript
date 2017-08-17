2c99c50d99357902c814f414b185d5fb85145708
Pete Chon <pete@llip.life>
2017-08-16 17:57:05 -0700

    test.js modified to test the add function in add.js

---
+++ javascriptAddTest/test.js
@@ -0,0 +1,10 @@
+const add = require ("./app.js");
+
+
+if (add (2, 3) === 5) {
+  console.log ("the answer is correct");
+} else {
+    console.log ("there is an error with your addition function");
+}
+
+
