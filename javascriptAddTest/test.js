f53f38240300f28620ced97a7e3b4d9433f79ea8
Pete Chon <pete@llip.life>
2017-08-16 18:28:20 -0700

    test function now receives two different inputs to test function in add.js

--- javascriptAddTest/test.js
+++ javascriptAddTest/test.js
@@ -1,10 +1,22 @@
 const add = require ("./app.js");
 
+const test = (value1, value2) => {
 
-if (add (2, 3) === 5) {
+  if (add(value1, value2) === (value1 + value2)) {
+    console.log ("the answer is correct");
+  } else {
+    console.log ("There is an error with your addition function");
+  }
+
+};
+
+test (2, 3);
+
+
+  /*if (add (2, 3) === 5) {
   console.log ("the answer is correct");
 } else {
     console.log ("there is an error with your addition function");
-}
+}*/
 
 
