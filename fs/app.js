af6a2c8319ccdc531122b19fc35faeb5a94dcab4
Pete Chon <pete@llip.life>
2017-08-17 18:43:26 -0700

    app.js creates a string of users and writes it to users.txt

--- fs/app.js
+++ fs/app.js
@@ -1,7 +1,24 @@
 const fs = require ("fs");
 
+let users = "";
+
 fs.readdir("/home", (error, data) =>{
 
-    console.log (data);
+  // Create a string with all of the users
+ 
+  data.forEach((user)=>{
+  
+    if (user != "pete") {
+      users = users + " " + user;
+    }
+  
+  });
+
+ // Write to user.txt file
+
+  fs.appendFile("users.txt", users);
+
+   
+
 
 });
